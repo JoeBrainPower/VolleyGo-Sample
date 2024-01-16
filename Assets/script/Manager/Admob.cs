e622f39b161316e545caab36c03f59b59dd0199b
JoeBrainPower <yiujoe@gmail.com>
2024-01-16 23:16:05 +0000

    Initial commit

---
+++ Assets/script/Manager/Admob.cs
@@ -0,0 +1,290 @@
+﻿using UnityEngine;
+using GoogleMobileAds.Api;
+using System;
+using System.Collections;
+using UnityEngine.UI;
+using System.Net;
+using System.IO;
+using UnityEngine.AI;
+
+public class Admob : MonoBehaviour
+{
+    [Header("BANNER")]
+    BannerView adBanner;
+    [Header("interstitial")]
+    public InterstitialAd interstitial;
+    public RewardedAd rewardedAd;
+    //bool TestMode = false;
+    public bool TestMode = false;
+
+#if UNITY_IPHONE || UNITY_IOS
+    //IOS Rewarded
+    const string RewardedAD_IOS_TEST_adUnitId = "ca-app-pub-3940256099942544/1712485313";
+    const string RewardedAD_IOS_adUnitId = "ca-app-pub-4028019648626827/8697951787";
+    // IOS Banner
+    const string Banner_IOS_TEST_adUnitId = "ca-app-pub-3940256099942544/2934735716";
+    const string Banner_IOS_adUnitId = "ca-app-pub-4028019648626827/1196179365";
+    //Interstitial
+    const string Interstitial_IOS_TEST_adUnitId = "ca-app-pub-3940256099942544/4411468910";
+    const string Interstitial_IOS_adUnitId = "ca-app-pub-4028019648626827/5257826225";
+#else
+    //ANDRROID Rewarded
+    const string RewardedAD_ANDROID_TEST_adUnitId = "ca-app-pub-3940256099942544/5224354917";
+    const string RewardedAD_ANDROID_adUnitId = "ca-app-pub-4028019648626827/5153081809";
+    //ANDRROID Banner
+    const string Banner_ANDROID_TEST_adUnitId = "ca-app-pub-3940256099942544/6300978111";
+    const string Banner_ANDROID_adUnitId = "ca-app-pub-4028019648626827/7423023472";
+    //Interstitial
+    const string Interstitial_TEST_ANDROID_adUnitId = "ca-app-pub-3940256099942544/5354046379";
+    const string Interstitial_ANDROID_adUnitId = "ca-app-pub-4028019648626827/3673281816";
+#endif
+
+
+    void Start()
+    {
+        MobileAds.Initialize(initStatus => { });
+    }
+
+    
+
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Banner~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    public void RequestBannerAd(AdPosition adPosition)
+    {
+#if UNITY_ANDROID || UNITY_EDITOR
+        if (Application.platform == RuntimePlatform.IPhonePlayer)
+            return;
+
+        if (adBanner != null) DestroyBannerAd();
+        if (adBanner == null)
+            {
+                CreateBannerView(adPosition);
+            }
+            // create our request used to load the ad.
+            var adRequest = new AdRequest();
+            adRequest.Keywords.Add("unity-admob-sample");
+
+            // send the request to load the ad.
+            Debug.Log("Loading banner ad.");
+            adBanner.LoadAd(adRequest);
+#endif
+
+    }
+    void CreateBannerView(AdPosition adPosition)
+    {
+        int Ads_width = 320, Ads_Height = 50;
+        //int start_X = Mathf.RoundToInt(0);
+        //int start_Y = Mathf.RoundToInt( 50);
+        AdSize adSize = new AdSize(Ads_width, Ads_Height);
+        string adUnitId;
+#if UNITY_ANDROID
+        adUnitId = TestMode ? Banner_ANDROID_TEST_adUnitId : Banner_ANDROID_adUnitId;
+#elif UNITY_IPHONE
+        adUnitId = TestMode? Banner_IOS_TEST_adUnitId : Banner_IOS_adUnitId;
+#else
+        adUnitId = "unexpected_platform";
+#endif
+        adBanner = new BannerView(adUnitId, adSize, adPosition);
+        //adBanner = new BannerView(adUnitId, adSize, start_X, start_Y);
+
+        //Debug.Log($"Banner start_X: {adBanner.GetWidthInPixels()}, start_Y: {adBanner.GetHeightInPixels()}");
+
+    }
+    public void DestroyBannerAd()
+    {
+        if (adBanner != null)
+        {
+            adBanner.Destroy();
+            adBanner = null;
+        }
+    }
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Interstital~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    public void RequestInterstitial()
+    {
+#if UNITY_ANDROID
+        string adUnitId = Interstitial_ANDROID_adUnitId;
+#elif UNITY_IPHONE
+        string adUnitId = Interstitial_IOS_adUnitId;
+#else
+        string adUnitId = "unexpected_platform";
+#endif
+        if (interstitial != null && !interstitial.CanShowAd())
+        {
+            interstitial.Destroy();
+            interstitial = null;
+        }
+        // create our request used to load the ad.
+        var adRequest = new AdRequest();
+        adRequest.Keywords.Add("unity-admob-sample");
+
+        // send the request to load the ad.
+        InterstitialAd.Load(adUnitId, adRequest,
+            (InterstitialAd ad, LoadAdError error) =>
+            {
+                // if error is not null, the load request failed.
+                if (error != null || ad == null)
+                {
+                    Debug.LogError("interstitial ad failed to load an ad " + "with error : " + error);
+                    return;
+                }
+
+                Debug.Log("Interstitial ad loaded with response : " + ad.GetResponseInfo());
+
+                interstitial = ad;
+            });
+        interstitial.OnAdClicked += () =>
+        {
+            Time.timeScale = 1;
+            RequestInterstitial();
+
+        };
+        // Raised when the ad failed to open full screen content.
+        this.interstitial.OnAdFullScreenContentFailed += HandleOnAdFailedToLoad;
+        // Raised when an ad opened full screen content.
+        this.interstitial.OnAdFullScreenContentOpened += HandleOnAdOpened;
+        // Raised when the ad closed full screen content.
+        this.interstitial.OnAdFullScreenContentClosed += HandleOnAdClosed;
+    }
+    public void ShowInterstitial()
+    {
+         if (interstitial != null && interstitial.CanShowAd())
+        {
+            interstitial.Show();
+
+        }
+        else if (interstitial == null || !interstitial.CanShowAd())
+        {
+            RequestInterstitial();
+            //Try to plat **REWARDED ADS
+            if (rewardedAd != null && rewardedAd.CanShowAd()) 
+            {
+                ShowRewardAdsNow();//No need Check connection
+            }
+        }
+    }
+    public void HandleOnAdFailedToLoad(AdError error)
+    {
+        Time.timeScale = 1;
+        RequestInterstitial();
+    }
+    void HandleOnAdLoaded()
+    {
+    }
+    public void HandleOnAdOpened()
+    {
+
+    }
+
+    public void HandleOnAdClosed()
+    {
+        interstitial.Destroy();
+
+    }
+
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Reward Ads~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    public void CreateAndLoadRewardedAd()
+    {
+        if (rewardedAd != null)
+        {
+            rewardedAd.Destroy();
+            rewardedAd = null;
+        }
+#if UNITY_ANDROID
+        string adUnitId = RewardedAD_ANDROID_adUnitId;
+#elif UNITY_IPHONE
+        string adUnitId = TestMode? RewardedAD_IOS_TEST_adUnitId : RewardedAD_IOS_adUnitId;
+#else
+        string adUnitId = "unexpected_platform";
+#endif
+        // create our request used to load the ad.
+        var adRequest = new AdRequest();
+        adRequest.Keywords.Add("unity-admob-sample");
+        // send the request to load the ad.
+        RewardedAd.Load(adUnitId, adRequest,
+            (RewardedAd ad, LoadAdError error) =>
+            {
+                // if error is not null, the load request failed.
+                if (error != null || ad == null)
+                {
+                    Debug.LogError("Rewarded ad failed to load an ad " +
+                                   "with error : " + error);
+                    return;
+                }
+                Debug.Log("Rewarded ad loaded with response : "
+                          + ad.GetResponseInfo());
+
+                rewardedAd = ad;
+            });
+        rewardedAd.OnAdFullScreenContentClosed += HandleRewardedAdClosed;
+        rewardedAd.OnAdFullScreenContentFailed += HandleRewardedAdFailedToShow;
+        rewardedAd.OnAdFullScreenContentOpened += HandleRewardedAdOpening;
+    }
+    public void HandleRewardedAdLoaded(object sender, EventArgs args)
+    {
+    }
+    public void HandleRewardedAdFailedToLoad()
+    {
+        Request_RewardAd();
+    }
+    public void HandleRewardedAdOpening()
+    {
+    }
+    public void HandleRewardedAdFailedToShow(AdError error)
+    {
+
+    }
+
+    public void HandleRewardedAdClosed()// Cut video
+    {
+        Request_RewardAd();
+    }
+    public void HandleUserEarnedReward(Reward reward)
+    {
+        Request_RewardAd();
+    }
+
+    public void Request_RewardAd()
+    {
+            Debug.Log("Request Rewarded ad ");
+        if (rewardedAd == null || !rewardedAd.CanShowAd())//Load rewardAD 1
+        {
+            CreateAndLoadRewardedAd();
+        }
+    }
+    public void ReloadVideo()//inspector button
+    {
+        Show_Rewarded_Ad();
+    }
+
+
+    public void Show_Rewarded_Ad()
+    {
+
+        if (rewardedAd != null && rewardedAd.CanShowAd())
+        {
+            ShowRewardAdsNow();
+        }
+        else if (interstitial != null && interstitial.CanShowAd())
+        {
+            ShowInterstitial();
+        }
+
+
+    }
+    void ShowRewardAdsNow()
+    {
+        if (rewardedAd != null && rewardedAd.CanShowAd())//try load reward 1
+        {
+            rewardedAd.Show((Reward reward) => { HandleUserEarnedReward(reward); });
+
+        }
+
+    }
+
+
+
+}
+
+
+
+
---
+++ Assets/script/Manager/Debug_Manager.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Debug_Manager : MonoBehaviour
+{
+
+    public Text info_Text_Board;
+    int pressTime = 0;
+    
+    public Admob Admob;
+    void Start()
+    {
+
+    }
+    public void SwitchAdsSupplier(bool on)
+    {
+        info_Text_Board.text = "SwitchAdsSupplier: " + (on?"UnityAds":"Admob");
+    }
+    public void AdmobTestModeOn(bool on)
+    {
+        Admob.TestMode = on;
+        info_Text_Board.text = "Admob Test Mode " + on;
+    }
+
+
+    public void AdMob_Initialize_info()
+    {
+        StopAllCoroutines();
+        StartCoroutine(AdMob_Status());
+    }
+    IEnumerator AdMob_Status()
+    {
+        info_Text_Board.text = "Checking\n";
+        if(Admob.interstitial != null)
+            info_Text_Board.text += "LOAD interstitial ADS : " + Admob.interstitial.CanShowAd().ToString() + "\n";
+        if(Admob.rewardedAd != null)
+            info_Text_Board.text += "LOAD Regarded ADS: " + Admob.rewardedAd.CanShowAd().ToString() + "\n";
+
+        yield return new WaitForSeconds(10f);
+        info_Text_Board.text = "";
+
+    }
+    public void Click_Load_AD()
+    {
+        StopAllCoroutines();
+        StartCoroutine(CheckSpeed_LoadAds());
+        Admob.Request_RewardAd();
+        Admob.RequestInterstitial();
+
+    }
+    public void Click_Play_Interstitial()
+    {
+        Admob.ShowInterstitial();
+    }
+    public void Click_Play_RewardAD()
+    {
+        Admob.Show_Rewarded_Ad();
+
+    }
+
+    IEnumerator CheckSpeed_LoadAds()
+    {
+        float startTime = Time.timeSinceLevelLoad;
+        float speed = 0;
+        info_Text_Board.text = "TestMode: " + Admob.TestMode + "\n";
+        info_Text_Board.text += "Start Loading ADS\n";
+
+        while ((Admob.interstitial != null && !Admob.interstitial.CanShowAd()) ||
+        (Admob.rewardedAd != null && !Admob.rewardedAd.CanShowAd()))
+        {
+            speed = Time.timeSinceLevelLoad - startTime;
+            info_Text_Board.text = "Loading ADS";
+            yield return new WaitForSeconds(0.5f);
+            info_Text_Board.text += ".";
+            yield return new WaitForSeconds(0.5f);
+            info_Text_Board.text += ".";
+            yield return new WaitForSeconds(0.5f);
+            info_Text_Board.text += ".";
+            yield return null;
+        }
+        info_Text_Board.text += "LOAD ADS done, Load time =" + speed.ToString("00") + " sec\n";
+        yield return new WaitForSeconds(10f);
+        info_Text_Board.text = "";
+
+    }
+    public void Click_CheckDate()
+    {
+        info_Text_Board.text = "";
+        info_Text_Board.text += "Now date time: " + DateTime.Now + "\n";
+        info_Text_Board.text += "Now date time ticks: " + DateTime.Now.Ticks + "\n";
+        DateTime Gift_Expire_Date;
+#if UNITY_IPHONE || UNITY_IOS || UNITY_EDITOR
+        Gift_Expire_Date = DateTime.Parse("27/12/2020");//dd/mm/yyyy
+#else
+        Gift_Expire_Date = DateTime.Parse("12/27/2020");//mm/dd/yyyy
+
+#endif
+        //if (Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.WindowsEditor)
+        //{
+        //    Gift_Expire_Date = DateTime.Parse("12/27/2020");//mm/dd/yyyy
+
+        //}
+        //else//IOS
+        //{
+        //    Gift_Expire_Date = DateTime.Parse("27/12/2020");//dd/mm/yyyy
+
+        //} 
+        info_Text_Board.text += "Expire_Date: " + Gift_Expire_Date + "\n";
+        info_Text_Board.text += "Expire_Date ticks: " + (ulong)Gift_Expire_Date.Ticks + "\n";
+        if ((ulong)DateTime.Now.Ticks < (ulong)Gift_Expire_Date.Ticks)
+        {
+            info_Text_Board.text += "Gift ready now!!" + "\n";
+        }
+        else
+        {
+            info_Text_Board.text += "Gift time is Expired!!" + "\n";
+        }
+    }
+}
